ab427bef921166c763cf7448203061cdbd05bdbb
rubens-lavor <haryson07@gmail.com>
2021-05-06 13:00:07 -0300

    classe mãe abstrata de incremento da força

---
+++ src/main/java/original/DecrementaForcaDaSenha.java
@@ -0,0 +1,24 @@
+package original;
+
+public abstract class DecrementaForcaDaSenha {
+    protected int peso = 0;
+    protected int contagem;
+    protected int bonus;
+
+    /*
+    public DecrementaForcaDaSenha() {
+        // super(senha);
+    }
+     */
+
+    public int obterBonus(){
+        return bonus;
+    }
+    public int obterContagem() {
+        return contagem;
+    }
+
+    public abstract void calculoContagem(String senha);
+    public abstract void calculoBonus(String senha);
+}
+
