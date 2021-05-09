ca2412817d27097a46130bbf5d472d302d1f8305
rubens-lavor <haryson07@gmail.com>
2021-05-09 17:40:14 -0300

    update

---
+++ src/main/java/original/ColecaoDeDependencias.java
@@ -0,0 +1,102 @@
+package original;
+
+import java.util.ArrayList;
+import java.util.EnumMap;
+import java.util.HashMap;
+import java.util.Map;
+
+public interface FabricaDependencias {
+
+        Map<String, Object> mapDependencias = new HashMap<>();
+        EnumMap<Contagem, Integer> mapContagem = new EnumMap<>(Contagem.class);
+        EnumMap<Bonus, Integer> mapBonus = new EnumMap<>(Bonus.class);
+
+        default Map<String, Object> retornarMap(){
+            return mapDependencias;
+        }
+        default EnumMap<Contagem, Integer> retornarMapContagem(){
+            return mapContagem;
+        }
+        default EnumMap<Bonus, Integer> retornarMapBonus(){
+            return mapBonus;
+        }
+
+        default int calcularSomatorioDeBonus(){
+            //int n = mapBonus.size();
+            int contagem = 0;
+            //for (int i = 0; i < n; i++) contagem += mapBonus.get(i);
+            for (EnumMap.Entry<Bonus, Integer> bonus : mapBonus.entrySet()) contagem += bonus.getValue();
+            return contagem;
+        }
+
+        public IncrementadorForcaDaSenha obj(String get);
+
+        default void AtribuirValoresMapContagemEBonus(){
+
+            mapContagem.put(Contagem.TAM_SENHA, IncrementadorForcaDaSenha.countLength);
+            mapContagem.put(Contagem.QNT_SIMBOLO, IncrementadorForcaDaSenha.countSymbol);
+            mapContagem.put(Contagem.QNT_NUMERO, IncrementadorForcaDaSenha.countNumber);
+            mapContagem.put(Contagem.QNT_LETRA_MINUSC, IncrementadorForcaDaSenha.countAlphaLC);
+            mapContagem.put(Contagem.QNT_LETRA_MAIUSC, IncrementadorForcaDaSenha.countAlphaUC);
+            mapContagem.put(Contagem.QNT_REQUERIMENTOS, IncrementadorForcaDaSenha.indiceRequerimentos);
+            mapContagem.put(Contagem.QNT_CARACTER_REPETIDO, IncrementadorForcaDaSenha.countRepChar);
+            mapContagem.put(Contagem.QNT_LETRA_MAIUSC_CONSECUTIVO, IncrementadorForcaDaSenha.countConsecutiveAlphaUC);
+            mapContagem.put(Contagem.QNT_LETRA_MINUSC_CONSECUTIVO, IncrementadorForcaDaSenha.countConsecutiveAlphaLC);
+            mapContagem.put(Contagem.QNT_NUMERO_CONSECUTIVO, IncrementadorForcaDaSenha.countConsecutiveNumber);
+            mapContagem.put(Contagem.QNT_SEQUENCIA_LETRA, IncrementadorForcaDaSenha.countSeqAlpha);
+            mapContagem.put(Contagem.QNT_SEQUENCIA_NUMERO, IncrementadorForcaDaSenha.countSeqNumber);
+            mapContagem.put(Contagem.QNT_SEQUENCIA_SIMBOLO, IncrementadorForcaDaSenha.countSeqSymbol);
+            mapContagem.put(Contagem.QNT_NUMERO_OU_SIMBOLO_NO_MEIO,IncrementadorForcaDaSenha.countNumerAndSymbol);
+
+            var y = mapContagem.get(Contagem.QNT_CARACTER_REPETIDO);
+
+            int countSymbol = IncrementadorForcaDaSenha.countSymbol;
+            int countNumber = IncrementadorForcaDaSenha.countNumber;
+            int countLength = IncrementadorForcaDaSenha.countLength;
+            int countAlphaLC = IncrementadorForcaDaSenha.countAlphaLC;
+            int countAlphaUC = IncrementadorForcaDaSenha.countAlphaUC;
+
+            if ((countSymbol == 0 && countNumber == 0)) {
+                mapContagem.put(Contagem.APENAS_LETRAS, countLength);
+                mapBonus.put(Bonus.APENAS_LETRAS, - IncrementadorForcaDaSenha.countLength);
+            } else {
+                mapContagem.put(Contagem.APENAS_LETRAS, 0);
+                mapBonus.put(Bonus.APENAS_LETRAS, 0);
+            }
+            if ((countAlphaLC == 0 && countAlphaUC == 0 && countSymbol == 0 && countNumber > 0)) {
+                mapContagem.put(Contagem.APENAS_NUMEROS, countLength);
+                mapBonus.put(Bonus.APENAS_NUMEROS, - IncrementadorForcaDaSenha.countLength);
+            } else {
+                mapContagem.put(Contagem.APENAS_NUMEROS, 0);
+                mapBonus.put(Bonus.APENAS_NUMEROS, 0);
+            }
+
+
+            /*Adicions bonus*/
+            mapBonus.put(Bonus.TAM_SENHA, IncrementadorForcaDaSenha.countLength*4);
+            mapBonus.put(Bonus.QNT_LETRA_MAIUSC, obj("IPNDCSMa").bonus);
+            mapBonus.put(Bonus.QNT_LETRA_MINUSC, obj("IPNDCSMi").bonus);
+            mapBonus.put(Bonus.QNT_NUMERO, IncrementadorForcaDaSenha.countNumber * 4);
+            mapBonus.put(Bonus.QNT_NUMERO_OU_SIMBOLO_NO_MEIO, obj("IPNDCENMS").bonus);
+            mapBonus.put(Bonus.QNT_SIMBOLO, IncrementadorForcaDaSenha.countSymbol * 6);
+            mapBonus.put(Bonus.QNT_REQUERIMENTOS, obj("IPNDR").bonus);
+
+
+            /*Deductions bonus*/
+            if (0 != IncrementadorForcaDaSenha.countRepChar) {
+                mapBonus.put(Bonus.QNT_CARACTER_REPETIDO, - (int) IncrementadorForcaDaSenha.incrementDeductionOfRepeatedChars);
+            }else {
+                mapBonus.put(Bonus.QNT_CARACTER_REPETIDO, 0);
+            }
+
+            mapBonus.put(Bonus.QNT_LETRA_MAIUSC_CONSECUTIVO, - IncrementadorForcaDaSenha.countConsecutiveAlphaUC * 2);
+            mapBonus.put(Bonus.QNT_LETRA_MINUSC_CONSECUTIVO, - IncrementadorForcaDaSenha.countConsecutiveAlphaLC * 2);
+            mapBonus.put(Bonus.QNT_NUMERO_CONSECUTIVO, - IncrementadorForcaDaSenha.countConsecutiveNumber * 2);
+
+            mapBonus.put(Bonus.QNT_SEQUENCIA_LETRA, - IncrementadorForcaDaSenha.countSeqAlpha * 3);
+            mapBonus.put(Bonus.QNT_SEQUENCIA_NUMERO, - IncrementadorForcaDaSenha.countSeqNumber * 3);
+            mapBonus.put(Bonus.QNT_SEQUENCIA_SIMBOLO, - IncrementadorForcaDaSenha.countSeqSymbol * 3);
+
+        }
+    }
+
---
+++ src/main/java/original/CriadorDasDependencias.java
@@ -0,0 +1,2 @@
+package original;public class CriadorDasDependencias {
+}
